81bbe13d811ccb6875a024b3ab138329f7c3284c
TheGatesScratch <mtkarels@outlook.com>
2023-04-05 17:02:21 +0200

    Remove item event code.

--- src/main/java/io/github/thegatesdev/witheronia/maze_gm/modules/item/MazeItemModule.java
+++ src/main/java/io/github/thegatesdev/witheronia/maze_gm/modules/item/MazeItemModule.java
@@ -1,9 +1,6 @@
 package io.github.thegatesdev.witheronia.maze_gm.modules.item;
 
-import io.github.thegatesdev.actionable.factory.ReactorFactory;
-import io.github.thegatesdev.eventador.EventData;
 import io.github.thegatesdev.eventador.EventListener;
-import io.github.thegatesdev.eventador.MappedReactors;
 import io.github.thegatesdev.maple.data.DataMap;
 import io.github.thegatesdev.maple.exception.ElementException;
 import io.github.thegatesdev.mapletree.data.Readable;
@@ -18,10 +15,6 @@ import io.github.thegatesdev.witheronia.maze_gm.util.DataFileLoader;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.event.Event;
-import org.bukkit.event.entity.EntityDropItemEvent;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.*;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -39,35 +32,29 @@ public class MazeItemModule extends PluginModule<MazeGamemode> implements EventL
             .add("lore", MazeDataTypes.COLORED_STRING.list(), null);
 
     private final ItemGroup itemGroup = new ItemGroup("maze_items", plugin.key("maze_item"), true);
-    private final EventData<ItemStack> itemEvents;
-    private final MappedReactors<ItemStack, String> itemReactors;
 
     public MazeItemModule(final MazeGamemode plugin) {
         super("items", plugin);
-        itemReactors = new MappedReactors<>(plugin.eventManager(), itemEvents, itemGroup::itemId);
         plugin.onDataFileLoad(this);
         plugin.listenerManager().add(this);
         plugin.stacker().itemManager().addGroup(itemGroup);
         this.logger = plugin.getLogger();
     }
 
-    {
-        itemEvents = new EventData<ItemStack>(plugin.eventManager()).add("used_stack", PlayerInteractEvent.class, PlayerInteractEvent::getItem)
-                .add("dropped_stack", PlayerDropItemEvent.class, event -> event.getItemDrop().getItemStack())
-                .add("dropped_stack", EntityDropItemEvent.class, event -> event.getItemDrop().getItemStack())
-                .add("picked_up_stack", EntityPickupItemEvent.class, event -> event.getItem().getItemStack())
-                .add("broken_stack", PlayerItemBreakEvent.class, PlayerItemBreakEvent::getBrokenItem)
-                .add("consumed_stack", PlayerItemConsumeEvent.class, PlayerItemConsumeEvent::getItem)
-                .add("new_stack", PlayerItemHeldEvent.class, event -> event.getPlayer().getInventory().getItem(event.getNewSlot()))
-                .add("old_stack", PlayerItemHeldEvent.class, event -> event.getPlayer().getInventory().getItem(event.getPreviousSlot()))
-                .add("main_hand_stack", PlayerSwapHandItemsEvent.class, PlayerSwapHandItemsEvent::getMainHandItem)
-                .add("off_hand_stack", PlayerSwapHandItemsEvent.class, PlayerSwapHandItemsEvent::getOffHandItem);
-    }
-
     // -- GET / SET
 
     public ItemGroup itemGroup() {
         return itemGroup;
+    }
+
+    @Override
+    public <E extends Event> boolean callEvent(@NotNull final E event, final Class<E> type) {
+        return false;
+    }
+
+    @Override
+    public Set<Class<? extends Event>> eventSet() {
+        return null;
     }
 
 
@@ -102,20 +89,10 @@ public class MazeItemModule extends PluginModule<MazeGamemode> implements EventL
         options.ifPrimitive("name", primitive -> builder.name(primitive.valueUnsafe()));
         options.ifPrimitive("lore", primitive -> builder.lore(primitive.<List<Component>>valueUnsafe()));
         options.ifPrimitive("reactors", primitive -> {
-            for (final ReactorFactory<?>.ReadReactor reactor : primitive.<List<ReactorFactory<?>.ReadReactor>>valueUnsafe())
-                itemReactors.map(itemId, reactor.eventClass(), reactor);
+            //for (final ReactorFactory<?>.ReadReactor reactor : primitive.<List<ReactorFactory<?>.ReadReactor>>valueUnsafe())
+            //itemReactors.map(itemId, reactor.eventClass(), reactor);
         });
         return new CustomItem(itemId, builder);
     }
 
-    @Override
-    public <E extends Event> boolean callEvent(@NotNull final E e, final Class<E> eventClass) {
-        if (!isEnabled) return false;
-        return itemReactors.callEvent(e, eventClass);
-    }
-
-    @Override
-    public Set<Class<? extends Event>> eventSet() {
-        return itemReactors.eventSet();
-    }
 }
