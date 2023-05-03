ab23679d030b11e5c3a15285c912d22d424ad0bc
TheGatesScratch <mtkarels@outlook.com>
2023-05-03 16:15:00 +0200

    Add pluginJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,4 +58,9 @@ tasks {
             include(dependency("io.github.thegatesdev:maze-generator"))
         }
     }
+
+    register<Copy>("pluginJar"){
+        from(shadowJar)
+        into(buildDir.resolve("pluginJar"))
+    }
 }
