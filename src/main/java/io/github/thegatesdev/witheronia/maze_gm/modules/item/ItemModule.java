0a6fce641b103b0e7e2f2283f645df68b829e66d
TheGatesScratch <mtkarels@outlook.com>
2023-08-05 22:41:08 +0200

    Remove item module code :)

--- src/main/java/io/github/thegatesdev/witheronia/maze_gm/modules/item/ItemModule.java
+++ src/main/java/io/github/thegatesdev/witheronia/maze_gm/modules/item/ItemModule.java
@@ -1,6 +1,5 @@
 package io.github.thegatesdev.witheronia.maze_gm.modules.item;
 
-import io.github.thegatesdev.maple.read.struct.DataType;
 import io.github.thegatesdev.stacker.item.ItemGroup;
 import io.github.thegatesdev.threshold.pluginmodule.ModuleManager;
 import io.github.thegatesdev.threshold.pluginmodule.PluginModule;
@@ -8,23 +7,9 @@ import io.github.thegatesdev.witheronia.maze_gm.MazeGamemode;
 
 public class ItemModule extends PluginModule<MazeGamemode> {
     private final ItemGroup itemGroup = plugin.itemManager().addGroup("maze");
-    private final DataType<ItemDataType.ReadItem> dataType = new ItemDataType();
 
     public ItemModule(ModuleManager<MazeGamemode> moduleManager) {
         super("items", moduleManager);
-    }
-
-    @Override
-    protected void onInitialize() {
-        plugin.EVENT_LOAD_DATAFILE.bind(this::onDataFileLoad);
-    }
-
-
-    private void onDataFileLoad(MazeGamemode.LoadDataFileInfo info) {
-        info.data().ifList("maze_items", list -> list.each(element -> {
-            var item = dataType.read(element);
-            itemGroup.add(item.key(), item.builder());
-        }));
     }
 
 
