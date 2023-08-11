364f9398bb770b3f0f8284f99fad45a75e14d6fb
TheGatesScratch <mtkarels@outlook.com>
2023-08-11 17:49:23 +0200

    Clean up command handling.

---
+++ src/main/java/io/github/thegatesdev/witheronia/maze_gm/command/Command.java
@@ -0,0 +1,26 @@
+package io.github.thegatesdev.witheronia.maze_gm.command;
+
+import dev.jorel.commandapi.CommandTree;
+import dev.jorel.commandapi.arguments.LiteralArgument;
+import dev.jorel.commandapi.executors.CommandExecutor;
+import io.github.thegatesdev.witheronia.maze_gm.MazeGamemode;
+
+public class Command extends CommandTree {
+
+    private final MazeGamemode gamemode;
+
+    public Command(MazeGamemode gamemode) {
+        super("scourge");
+        this.gamemode = gamemode;
+        withAliases("sc");
+        addDefaults();
+    }
+
+    private void addDefaults() {
+        then(cmdReload(gamemode));
+    }
+
+    private static LiteralArgument cmdReload(MazeGamemode mazeGamemode) {
+        return (LiteralArgument) new LiteralArgument("reload").executes((CommandExecutor) (sender, args) -> mazeGamemode.reload());
+    }
+}
